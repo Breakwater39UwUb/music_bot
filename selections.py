210674beefdbfd235dc3a5f333e3255f6de6b722
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-10-07 15:49:08 +0800

    Update renamed file for selections

--- selections.py
+++ selections.py
@@ -11,5 +11,5 @@ class SongTags(discord.ui.Select):
         options = [discord.SelectOption(label=tag) for tag in tags]
         super().__init__(placeholder=placeholder, min_values=0, options=options)
     async def callback(self, interaction: discord.Interaction):
-        # if self.values[0] == "Option 3":
+
         await interaction.response.send_message(self.values[0],ephemeral=True)
