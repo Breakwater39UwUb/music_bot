ae768d3e46a8d818a94d5b3a4a4df3e592843510
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-10-28 13:24:38 +0800

    Update

--- cogs/music.py
+++ cogs/music.py
@@ -26,7 +26,6 @@ class Music(commands.Cog):
                 options = [discord.SelectOption(label=tag) for tag in tags]
                 super().__init__(placeholder=placeholder, min_values=0, max_values=len(options), options=options)
 
-        '''宣告一個 ViewClass 類別，繼承 discord.ui.View'''
         def __init__(self, timeout: float | None = 180):
         # def __init__(self, songData, timeout: float | None = 180):
             super().__init__(timeout = timeout)
@@ -143,7 +142,7 @@ class Music(commands.Cog):
     @app_commands.describe(song_title='song title', artist='artist name, if you don\'t know the name, skip it or type "UNKNOWN"', url='song url')
     async def share_song_manual(self, interaction: discord.Interaction,
                          song_title: str, artist: str = '5529', url: str = ''):
-        # TODO: Try to auto fetch song title and artist
+        # TODO: Optimize manual input experience
         user = (interaction.user.id, interaction.user.name)
         try:
             await interaction.response.defer(ephemeral=True, thinking = True)
@@ -165,22 +164,20 @@ class Music(commands.Cog):
         try:
             await interaction.response.defer(ephemeral=True, thinking = True)
             song_args = self.parse_song(link)
-            if db.find_artists(song_args[1]) == []:
-                await self.add_artist(interaction)
+            artistResult = db.find_artists(song_args[1])
+            if artistResult == []:
+                await self.add_artist(interaction, song_args[1])
             song_attr = tuple(db.submit_song(song_args[0], song_args[1], user))
             song_attr += (link, song_args[2])
             view = self.TagSelMenu()
             view.songData = song_attr
             await interaction.followup.send(view = view)
         except Exception as e:
-            err_msg = f'Failed to submit song.\n{e}'
+            err_msg = f'Failed to submit song.\n```{e}```'
             await interaction.followup.send(err_msg, ephemeral=True)
 
-    async def add_artist(self, iact: discord.Interaction):
-        await iact.followup.send('It looks that the artist is not in our database.\nType the Artist Name to add one.')
-        message = await self.bot.wait_for("message", timeout=60.0)
-        artistName = message.content
-
+    async def add_artist(self, iact: discord.Interaction, artistName):
+        await iact.followup.send(f'It looks that the artist, `{artistName}` is not in our database.\nWe are adding this one.')
         await iact.followup.send('Type the Artist Altnate Name, like his name in other languages.(type `-none` to skip)')
         message = await self.bot.wait_for("message", timeout=60.0)
         artistAltName = message.content if message.content != '-none' else None
@@ -188,13 +185,14 @@ class Music(commands.Cog):
         await iact.followup.send('Which record company this artist signed to?(type `-none` to skip)')
         message = await self.bot.wait_for("message", timeout=60.0)
         artistCompany = message.content if message.content != '-none' else None
-        companyResults = db.find_company(artistCompany) if companyResults is not None else []
+        companyResults = db.find_company(artistCompany) if artistCompany is not None else []
         if companyResults == []:
             await self.add_company(iact, artistCompany)
         else:
             view = self.CompanySelMenu(companyResults)
             await iact.followup.send(view=view)
-            
+            wait = await self.bot.wait_for('interaction', timeout=60)
+            artistCompany = wait
 
         db.insert_to_table((artistName, artistAltName, artistCompany), db.TABLES['artists'])
 
@@ -219,8 +217,11 @@ class Music(commands.Cog):
         # TODO Implement YouTubeMusic
         if 'youtube' in songLink or 'youtu.be' in songLink:
             yt = YTMusic('oauth.json')
-            song = self.get_thumbnail(songLink)[2]
-            search_results = yt.search(query=song, limit=2)
+            # TODO check return value from YouTubeMusic
+            # TODO implement albums detection (playlist)
+            # Consider move url parsing to this function
+            linkArgs = self.parse_link(songLink)
+            search_results = yt.search(query=linkArgs, limit=2)
             title = search_results[0]['title']
             artist = search_results[0]['artists'][0]['name']
             thumbnail = search_results[0]['thumbnails'][-1]['url']
@@ -229,20 +230,21 @@ class Music(commands.Cog):
         # TODO Implement Spotify
 
         # TODO Implement Tidal
+        # Tidal API requires account login to use, but it is not available in Taiwan
         # https://developer.tidal.com/documentation/api-sdk/api-sdk-quick-start
         # https://developer.tidal.com/apiref
 
         # TODO Implement Apple Music
 
-    def get_thumbnail(self, url):
-        # TODO: Add Apple Music and Tidal support
-        # Parse the URL to get the query parameters
+    def parse_link(self, url):
+        '''Parse the URL to get the query parameters'''
+        # TODO: Add Apple Music and Tidal support, this function may merge to parse_song() in the future
         parsed_url = urlparse(url)
         query_params = parse_qs(parsed_url.query)
 
         # Case 1: Standard YouTube URL (e.g., https://www.youtube.com/watch?v=SdDdyMb0p2U)
         if "v" in query_params:
-            return url, f'https://img.youtube.com/vi/{query_params["v"][0]}/maxresdefault.jpg', query_params["v"][0]
+            return query_params["v"][0]
 
         # Case 2: Google redirect URL (e.g., https://www.google.com/url?...&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DSdDdyMb0p2U&...)
         if "url" in query_params:
@@ -250,12 +252,18 @@ class Music(commands.Cog):
             youtube_parsed = urlparse(youtube_url)
             youtube_query_params = parse_qs(youtube_parsed.query)
             if "v" in youtube_query_params:
-                return youtube_url, f'https://img.youtube.com/vi/{youtube_query_params["v"][0]}/maxresdefault.jpg', youtube_query_params["v"][0]
+                return youtube_query_params["v"][0]
 
         # Case 3: YouTube short URL (e.g., https://youtu.be/SdDdyMb0p2U)
         short_id_match = re.search(r"youtu\.be/([^?&]+)", url)
         if short_id_match:
-            return url, f'https://img.youtube.com/vi/{short_id_match.group(1)}/maxresdefault.jpg', short_id_match.group(1)
+            return short_id_match.group(1)
+
+        if parsed_url.path == '/playlist':
+            return query_params['list']
+
+        if parsed_url.hostname == 'music.youtube.com':
+            return url
 
         if 'spotify' in url:
             prefix = "https://open.spotify.com/oembed?url="
@@ -268,7 +276,43 @@ class Music(commands.Cog):
             return url, data['thumbnail_url']
         return None
 
-    def format_url(self, data):
+    def get_thumbnail(self, url):
+            '''Parse the URL to get the query parameters'''
+            # TODO: Add Apple Music and Tidal support, this function may merge to parse_song() in the future
+            parsed_url = urlparse(url)
+            query_params = parse_qs(parsed_url.query)
+
+            # Case 1: Standard YouTube URL (e.g., https://www.youtube.com/watch?v=SdDdyMb0p2U)
+            if "v" in query_params:
+                return url, f'https://img.youtube.com/vi/{query_params["v"][0]}/maxresdefault.jpg', query_params["v"][0]
+
+            # Case 2: Google redirect URL (e.g., https://www.google.com/url?...&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DSdDdyMb0p2U&...)
+            if "url" in query_params:
+                youtube_url = query_params["url"][0]
+                youtube_parsed = urlparse(youtube_url)
+                youtube_query_params = parse_qs(youtube_parsed.query)
+                if "v" in youtube_query_params:
+                    return youtube_url, f'https://img.youtube.com/vi/{youtube_query_params["v"][0]}/maxresdefault.jpg', youtube_query_params["v"][0]
+
+            # Case 3: YouTube short URL (e.g., https://youtu.be/SdDdyMb0p2U)
+            short_id_match = re.search(r"youtu\.be/([^?&]+)", url)
+            if short_id_match:
+                return url, f'https://img.youtube.com/vi/{short_id_match.group(1)}/maxresdefault.jpg', short_id_match.group(1)
+
+            if parsed_url.hostname == 'music.youtube.com':
+                return 
+            if 'spotify' in url:
+                prefix = "https://open.spotify.com/oembed?url="
+                thumbnail_url = f"{prefix}{url}"
+
+                x = request.urlopen(thumbnail_url)
+                raw_data = x.read()
+                encoding = x.info().get_content_charset('utf8')  # JSON default
+                data = json.loads(raw_data.decode(encoding))
+                return url, data['thumbnail_url']
+            return None
+
+    def format_url(data):
         base_url = "https://www.google.com/search?"
         params = {"q": data}
 
