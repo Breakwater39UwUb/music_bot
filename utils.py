dde566b09e6fac201284934f0727304072af90b0
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-11-29 10:20:15 +0800

    Update MyLogger class

--- utils.py
+++ utils.py
@@ -2,91 +2,6 @@ import re
 import logging
 import os
 
-invalid_chars = '<>:"/\|?*｜\n. '
-invalid_char_pattern = '|'.join(map(re.escape, invalid_chars))
-webname_filter = re.compile(invalid_char_pattern)
-'''re object with pattern: '<>:"/\|?*｜\\n .\''''
-
-url_regex = re.compile(r"^(?P<protocol>[a-z\-]+)?:?(?P<slash>\/\/)?(?P<host>[a-zA-Z0-9\./]+)?(?P<port>:\d+)?$")
-'''re object with pattern to filter url'''
-
-en_str_regex = re.compile(r'[a-zA-Z0-9_.:/-]+')
-'''re object with pattern to filter english string'''
-
-lineid_regex = re.compile(r'[a-zA-Z0-9@_.-]+')
-'''re object with pattern to filter english string'''
-
-idcard_regex = re.compile(r'[A-Z][1289][0-9]{8}(?![0-9])')
-'''re object with pattern to filter ID card number'''
-
-# TODO: Add function documentation
-def parse_url(text: str):
-    '''Parse url from string
-    
-    '''
-
-    urls = []
-    text = sep_english(text)
-    words = text.split()
-    
-    for part in words:
-        match = url_regex.match(part)
-        if match and match.group('protocol') and match.group('host'):
-            # print(f"protocol {match.group('protocol')}")  # Outputs: http
-            # print(f"host {match.group('host')}")  # Outputs: example.com
-            # print(f"port {match.group('port')}")  # Outputs: :80
-            urls.append(part)
-    
-    return urls
-
-def parse_lineid(input_string: str):
-    '''Parse Line ID from string
-    
-    '''
-
-    words  = lineid_regex.findall(input_string)
-
-    for line_id in words:
-        input_string = input_string.replace(line_id, f' {line_id} ', 1)
-
-    return input_string.split()
-
-def sep_english(input_string: str):
-    '''Apped head and end with spaces to english string'''
-    # Use regular expression to find English words and URLs
-    english_words = en_str_regex.findall(input_string)
-
-    # Add space before and after each English word
-    for word in english_words:
-        input_string = input_string.replace(word, f' {word} ', 1)
-
-    return input_string
-
-def is_contain_IDcard(input_string: str):
-    '''Check if a string contains ID card number.'''
-
-    id_number = idcard_regex.search(input_string)
-    if id_number is not None:
-        if validate_IDcard(id_number.group(0)):
-            return True
-    return False
-
-def validate_IDcard(ID: str):
-    '''Validate ID card number'''
-    validate_weight = [1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1]
-    checksum = 0
-    base = ord('A')
-    special_cases = {'I': 34, 'O': 35, 'W': 32, 'X': 30, 'Y': 31, 'Z': 33}
-
-    n0 = special_cases.get(ID[0], ord(ID[0]) - base + 10)
-    ID = ID.replace(ID[0], str(n0))
-
-    for index in range(0, len(ID)):
-        checksum += int(ID[index]) * validate_weight[index]
-    if checksum%10 != 0:
-        return False
-    return True
-
 def print_directory_structure(root_dir, ignore_dirs=None):
     """
     Prints the directory structure of the given root directory, ignoring specified directories.
@@ -135,10 +50,12 @@ def process_names(name: str, altName: str):
 
     return altName
 
-class Debug_Logger:
+# class GuildProfileReader:
+
+class My_Logger:
     '''Custom logger class'''
 
-    def __init__(self, logger_name, log_level=logging.DEBUG, filename='logs/debug.log'):
+    def __init__(self, logger_name, log_level=logging.DEBUG, filename='debug'):
         '''
         logger_name: name of logger
         log_level: logging level
@@ -154,6 +71,7 @@ class Debug_Logger:
         # Check if the logger already has handlers
         if not self.logger.hasHandlers():
             self._setup_console_handler()
+            filename = f'logs/{filename}.log'
             self._setup_file_handler(filename)
         self.logger.propagate = False
 
