37daad18e7866628fe37c5adfea743cd9ce5f4e0
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-11-30 14:19:42 +0800

    Add alternative functions

--- cogs/cog_manager.py
+++ cogs/cog_manager.py
@@ -5,6 +5,11 @@ from typing import Optional
 import discord
 from discord import app_commands
 from discord.ext import commands
+import discord.ext
+import discord.ext.commands
+import utils
+
+log = utils.My_Logger(__file__, 20, filename='bot')
 
 class BotManger(commands.Cog):
     class CogModules(enum.Enum):
@@ -52,9 +57,46 @@ class BotManger(commands.Cog):
             )
         except asyncio.TimeoutError:
             await interaction.followup.send(
-                f'Error: Loading `{module.value}` timed out after 20 seconds.',
+                f'Error: Loading `{module.value}` timed out.',
                 ephemeral=True
             )
+
+    async def load_from_web(self, module):
+        result = {'feature': os.path.basename(__file__),
+                  'method': self.load_from_web.__name__,
+                  'cog_file': module,
+                  'result': 'Fail'}
+        try:
+            if module == self.CogModules.all:
+                for item in self.CogModules:
+                    if item == self.CogModules.all:
+                        continue
+                    # Reload each module with a 20-second timeout
+                    await asyncio.wait_for(self.bot.load_extension(item), timeout=20)
+            else:
+                # Reload the specified module with a 20-second timeout
+                await asyncio.wait_for(
+                    self.bot.load_extension(f'{self.CogModules.__cogDir__}{module}'),
+                    timeout=20)
+
+            slash = await self.bot.tree.sync()
+            result['result'] = 'Success'
+            log.log(f'`{module}` loaded.')
+            log.log(f"載入 {len(slash)} 個斜線指令")
+            return result
+        except asyncio.TimeoutError as te:
+            log.log(f'Loading `{module}` timed out.',40)
+            result['error'] = te.__class__.__name__
+            return result
+        except discord.ext.commands.ExtensionNotFound as extnf:
+            log.log(f'Module `{module}` not found.',40)
+            result['error'] = extnf.__class__.__name__
+            return result
+        except Exception as e:
+            log.log(f'Loading `{module}`, {e}',40)
+            return result
+        except:
+            raise
 
     @app_commands.command(
         name='unload_module',
@@ -85,9 +127,44 @@ class BotManger(commands.Cog):
             )
         except asyncio.TimeoutError:
             await interaction.followup.send(
-                f'Error: Unloading `{module.value}` timed out after 20 seconds.',
+                f'Error: Unloading `{module.value}` timed out.',
                 ephemeral=True
             )
+
+    async def unload_from_web(self, module):
+        result = {'feature': os.path.basename(__file__),
+                  'method': self.unload_from_web.__name__,
+                  'cog_file': module,
+                  'result': 'Fail'}
+        try:
+            if module == self.CogModules.all:
+                for item in self.CogModules:
+                    if item == self.CogModules.all:
+                        continue
+                    # Reload each module with a 20-second timeout
+                    await asyncio.wait_for(self.bot.unload_extension(item), timeout=20)
+            else:
+                # Reload the specified module with a 20-second timeout
+                await asyncio.wait_for(
+                    self.bot.unload_extension(f'{self.CogModules.__cogDir__}{module}'),
+                    timeout=20)
+
+            slash = await self.bot.tree.sync()
+            result['result'] = 'Success'
+            log.log(f'`{module}` loaded.')
+            log.log(f"載入 {len(slash)} 個斜線指令")
+            return result
+        except asyncio.TimeoutError as te:
+            log.log(f'Unloading `{module}` timed out.',40)
+            result['error'] = te.__class__.__name__
+            return result
+        except discord.ext.commands.ExtensionNotFound as extnf:
+            log.log(f'Module `{module}` not found.',40)
+            result['error'] = extnf.__class__.__name__
+            return result
+        except Exception as e:
+            log.log(f'Unloading `{module}`, {e}', 40)
+            return result
 
     @app_commands.command(
         name='reload_module',
@@ -120,9 +197,53 @@ class BotManger(commands.Cog):
             )
         except asyncio.TimeoutError:
             await interaction.followup.send(
-                f'Error: Reloading `{module.value}` timed out after 20 seconds.',
+                f'Error: Reloading `{module.value}` timed out.',
                 ephemeral=True
             )
+
+    async def reload_from_web(self, module):
+        result = {'feature': os.path.basename(__file__),
+                  'method': self.reload_from_web.__name__,
+                  'cog_file': module,
+                  'result': 'Fail'}
+        try:
+            if module == self.CogModules.all:
+                for item in self.CogModules:
+                    if item == self.CogModules.all:
+                        continue
+                    # Reload each module with a 20-second timeout
+                    await asyncio.wait_for(self.bot.reload_extension(item), timeout=20)
+            else:
+                # Reload the specified module with a 20-second timeout
+                await asyncio.wait_for(
+                    self.bot.reload_extension(f'{self.CogModules.__cogDir__}{module}'),
+                    timeout=20)
+
+            slash = await self.bot.tree.sync()
+            result['result'] = 'Success'
+            log.log(f'`{module}` reloaded.')
+            log.log(f"載入 {len(slash)} 個斜線指令")
+            return result
+        except asyncio.TimeoutError as te:
+            log.log(
+                f'Reloading `{module}` timed out.',
+                40
+            )
+            result['error'] = te.__class__.__name__
+            return result
+        except discord.ext.commands.ExtensionNotFound as extnf:
+            log.log(
+                f'Module `{module}` not found.',
+                40
+            )
+            result['error'] = extnf.__class__.__name__
+            return result
+        except Exception as e:
+            log.log(
+                f'Reloading `{module}`, {e}',
+                40
+            )
+            return result
 
 async def setup(bot: commands.Bot):
     '''Cog 載入 Bot'''
