d412fb16074b0c4026f5cebbf9e302f1a291d02a
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-11-30 15:12:18 +0800

    Add restart command

--- cogs/main.py
+++ cogs/main.py
@@ -1,7 +1,13 @@
+import os
+import sys
 import discord
 from discord.ext import commands
 from discord import app_commands
 import db
+import utils
+
+log = utils.My_Logger(__file__, 20, filename='bot')
+cmd_log = utils.My_Logger(__file__, 20, filename='command history')
 
 # 定義名為 Main 的 Cog
 class Main(commands.Cog):
@@ -11,11 +17,25 @@ class Main(commands.Cog):
     # 前綴指令
     @commands.command()
     async def Hello(self, ctx: commands.Context):
+        cmd_log.log(f'{self.Hello.callback.__name__} called by {ctx.author}')
         await ctx.send("Hello, world!")
 
     @commands.command()
     async def ping(self, ctx: commands.Context):
+        cmd_log.log(f'{self.ping.callback.__name__} called by {ctx.author}')
         await ctx.send(f'Ping: {self.bot.latency*1000:.0f}ms')
+
+    def restart_bot(self): 
+        log.log('Restarting bot.')
+        os.execv(sys.executable, ['python'] + sys.argv)
+
+    @commands.command()
+    async def restart(self, ctx: commands.Context):
+        cmd_log.log(f'{self.restart.callback.__name__} called by {ctx.author}')
+
+        # TODO: Add if statement to verify that the command was authorized by guild profiles
+        await ctx.send("Restarting bot...")
+        self.restart_bot()
 
     @commands.command()
     async def DB(self, ctx: commands.Context, command, arg):
