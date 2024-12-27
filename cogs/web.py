e6feef4fc6c34f60268ce6d265f087257f995009
Breakwater39UwUb <leoandhankmail@gmail.com>
2024-12-27 09:40:28 +0800

    Port from fastAPI to flask

--- cogs/web.py
+++ cogs/web.py
@@ -1,197 +1,141 @@
-'''
-This COG file uses `aiohttp` to implement http communication.
-This web server can not restart along the COG reload method.
-'''
-
 import os
-import sys
 import json
-import aiohttp
-from aiohttp import web
+import asyncio
+from flask import Flask, jsonify, request, Response
 from discord.ext import commands, tasks
-import discord
+import utils
 from dataclass import (
     CogRequest,
     actionRequest,
     GuildProfile
 )
-import utils
 
-# TODO: log should record authorized users, now are "Unknown".
-# user may record in html header
+# 初始化日誌記錄器
 bot_log = utils.My_Logger(__file__, 20, filename='bot')
 cmd_log = utils.My_Logger(__file__, 20, filename='command history')
 
-# using fastAPI
-from fastapi import FastAPI, Response
-app = FastAPI()
+# 初始化 Flask
+flask_app = Flask(__name__)
 
 class Webserver(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.flask_port = int(os.environ.get('FLASK_PORT', 5887))
         self.web_server.start()
-        self.webserver_port = int(os.environ.get('PORT', 5000))
 
-        @app.get('/')
-        async def welcome():
-            return "Hello, world"
+        # Flask 路由設定
+        @flask_app.route('/')
+        def welcome():
+            return jsonify({"message": "Hello, world"}), 200
 
-        @app.get('/check_status')
-        async def check_status():
-            '''A get method to check whether the bot is dead or not.'''
+        @flask_app.route('/check_status', methods=['GET'])
+        def check_status():
+            '''檢查機器人是否在線。'''
             user = 'Unknown'
             cmd_log.log(f'{check_status.__name__} called by {user}')
             try:
                 status = self.bot.is_ready()
-                return Response(str(status))
+                return jsonify({"status": status}), 200
             except Exception as e:
-                return Response(f'Error: {e}', status_code=500)
+                return jsonify({"error": str(e)}), 500
 
-        @app.get('/get_guilds')
-        async def get_guilds():
-            '''A get method for guilds that bot are invited in.'''
+        @flask_app.route('/get_guilds', methods=['GET'])
+        def get_guilds():
+            '''取得機器人加入的伺服器列表。'''
             user = 'Unknown'
             cmd_log.log(f'{get_guilds.__name__} called by {user}')
             guilds = [guild for guild in self.bot.guilds]
-            guildList = [
+            guild_list = [
                 {'No.': i + 1, 'name': guild.name, 'id': guild.id}
                 for i, guild in enumerate(guilds)
             ]
-            guildList = json.dumps(guildList)
-            return Response(guildList, 200, media_type='application/json')
+            return jsonify(guild_list), 200
 
-        @app.post('/test')
-        async def dblwebhook(request):
-            # Ignore this, this is a example
-            if request.headers.get('authorization') == '3mErTJMYFt':
-                data = await request.json()
-            return 200
-
-        @app.post('/cog')
-        async def cog(request: CogRequest):
-            '''A post method for cog management'''
+        @flask_app.route('/cog', methods=['POST'])
+        def cog():
+            '''管理 COG 的載入、卸載與重新載入。'''
             user = 'Unknown'
             cmd_log.log(f'{cog.__name__} called by {user}')
-            cogManager = self.bot.get_cog('BotManger')
+            cog_manager = self.bot.get_cog('BotManger')
+            data = request.get_json()
 
-            if request.method == 'load':
-                result = await cogManager.load_from_web(module = request.file)
-            elif request.method == 'unload':
-                result = await cogManager.unload_from_web(module = request.file)
-            elif request.method =='reload':
-                result = await cogManager.reload_from_web(module = request.file)
+            if data['method'] == 'load':
+                result = asyncio.run(cog_manager.load_from_web(module=data['file']))
+            elif data['method'] == 'unload':
+                result = asyncio.run(cog_manager.unload_from_web(module=data['file']))
+            elif data['method'] == 'reload':
+                result = asyncio.run(cog_manager.reload_from_web(module=data['file']))
             else:
-                return Response('Invalid method', 400)
+                return jsonify({"error": "Invalid method"}), 400
 
-            if result['result'] == 'Fail':
-                code = 500
-            elif result['result'] == 'Success':
-                code = 200
+            code = 200 if result['result'] == 'Success' else 500
+            return jsonify(result), code
 
-            result = json.dumps(result, indent=4)
-            return Response(content = result, status_code = code, media_type='application/json')
-
-        @app.post('/bot_action')
-        async def bot_action(request: actionRequest):
-            '''A post action for bot action, ex: restart, logout'''
+        @flask_app.route('/bot_action', methods=['POST'])
+        def bot_action():
+            '''執行機器人相關操作，例如重啟或登出。'''
             user = 'Unknown'
-            if request.action == 'restart':
+            data = request.get_json()
+
+            if data['action'] == 'restart':
                 cog = self.bot.get_cog('Main')
-                # TODO: Handle no http response due to process restart.
                 cmd_log.log(f'restart called by {user}')
                 cog.restart_bot()
-            # TODO: implement other actions
 
-        # TODO: implement guild profile creation
-        @app.post('/gen_guild_profile')
-        async def gen_guild_profile(request: GuildProfile):
-            '''A post method for creating a guild profile.
-            Featured channel could be empty class.
+            return Response(status=200)
 
-            Before calling this api, user should choose a guild from guild list using `/get_guilds`
-            json structure as follows:
-            
-            ```json
-            {
-                "name": string,
-                "id": int,
-                "featured channel": {
-                    "spend_share": {
-                        "channel name": string,
-                        "channel_id": int
-                    },
-                    "music_share": {}
-                }
-            }
-            ```
-            '''
+        @flask_app.route('/gen_guild_profile', methods=['POST'])
+        def gen_guild_profile():
+            '''創建伺服器設定檔。'''
             user = 'Unknown'
             cmd_log.log(f'gen_guild_profile called by {user}')
-            confManager = self.bot.get_cog('GuildConfigManager')
+            conf_manager = self.bot.get_cog('GuildConfigManager')
+            data = request.get_json()
+
             try:
-                result = await confManager.save_guild_profile(request)
-                return Response(result, 200)
+                result = conf_manager.save_guild_profile(data)
+                return jsonify(result), 200
             except Exception as e:
                 bot_log.log(f'Error generating profile: {e}', 40)
-                return Response(f'Error: {e}', status_code=500)
+                return jsonify({"error": str(e)}), 500
 
-        # TODO: Figure out should use individual api for profile setting.
-        @app.post('/guild_profile_settings')
-        async def guild_profile_settings(request):
-            # TODO: implement guild_profile_settings
-            try:
-                return Response(request.action)
-            except:
-                # bot_log.log('Error parsing TC', 40)
-                return Response('Error parsing TC', status_code=500)
-
-        # TODO: Make parameter both accept json and null(no post data)
-        @app.post('/load_guild_profile')
-        async def load_guild_profile(guild_id: int = None):
-            '''A method for loading guild profile from json file.
-            
-            If guild_id is given, it will be load/reload the profile,
-            otherwise it will load all saved guild profiles.
-            
-            :param int guild_id: Discord guild id.'''
+        @flask_app.route('/load_guild_profile', methods=['POST'])
+        def load_guild_profile():
+            '''從 JSON 文件加載伺服器設定檔。'''
             user = 'Unknown'
             cmd_log.log(f'load_guild_profile called by {user}')
-            confManager = self.bot.get_cog('GuildConfigManager')
+            conf_manager = self.bot.get_cog('GuildConfigManager')
+            data = request.get_json()
+
             try:
-                if guild_id is not None:
-                    result = await confManager.load_guild_profile(guild_id)
-                else:
-                    result = await confManager.load_guild_profile()
-                result = await self.dataclass_to_json(result)
-                return Response(result, 200)
+                guild_id = data.get('guild_id')
+                result = conf_manager.load_guild_profile(guild_id) if guild_id else conf_manager.load_guild_profile()
+                result_json = Webserver.dataclass_to_json(result)
+                return jsonify(result_json), 200
             except Exception as e:
                 bot_log.log(f'Error loading profile: {e}', 40)
-                return Response(f'Error: {e}', status_code=500)
+                return jsonify({"error": str(e)}), 500
 
-    # TODO: implement change profile from json
-    # async def set_guild_profile(self, filename, option, changes):
-    #     '''A function that set the guild profile json file.'''
-    #     with open(filename, 'a'):
-    #         pass
-
-    async def dataclass_to_json(self, obj: dict[GuildProfile] | GuildProfile):
-        if type(obj) == dict:
+    @staticmethod
+    def dataclass_to_json(obj):
+        '''將 dataclass 轉換為 JSON 字串。'''
+        if isinstance(obj, dict):
             converted_dict = [prof.model_dump(mode='python', exclude='action') for prof in obj.values()]
-            json_str = json.dumps(converted_dict, ensure_ascii=False, indent=4)
+            return converted_dict
+        elif isinstance(obj, GuildProfile):
+            return obj.model_dump(mode='python', exclude='action')
+        return {}
 
-        # TODO: implement this method, convert GuildProfile to json string.
-        if type(obj) == GuildProfile:
-            converted_dict = obj.model_dump(mode='python', exclude='action')
-            json_str = json.dumps(converted_dict, ensure_ascii=False, indent=4)
-
-        return json_str
-
+            # TODO: Fix error occur after restart: OSError: [Errno 48] Address already in use
     @tasks.loop()
     async def web_server(self):
-        import uvicorn  # Import uvicorn here to avoid circular imports
-        config = uvicorn.Config(app, host="127.0.0.1", port=self.webserver_port)
-        server = uvicorn.Server(config)
-        await server.serve()
+        from threading import Thread
+
+        def start_flask():
+            flask_app.run(host="0.0.0.0", port=self.flask_port)
+
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(None, start_flask)
 
     @web_server.before_loop
     async def web_server_before_loop(self):
